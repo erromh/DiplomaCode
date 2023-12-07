3641b66f6514d03f258db0a83df0b2d32bde6f38
errommore995@gmail.com <errommore995@gmail.com>
2023-12-07 20:42:55 +0600

    regression

--- app.py
+++ app.py
@@ -1,25 +1,44 @@
-from dash import Dash, html, dcc, callback, Output, Input
+import numpy as np
 import plotly.express as px
-import pandas as pd
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
+# Генерация случайных данных для примера
+np.random.seed(42)
+X = np.random.rand(100, 1) * 10
+y = 2 * X + 1 + np.random.randn(100, 1) * 2
 
-app = Dash(__name__)
+# Применение метода наименьших квадратов для простой линейной регрессии
+coefficients = np.polyfit(X.flatten(), y.flatten(), 1)
+slope, intercept = coefficients
 
+# Создание предсказаний для графика
+x_range = np.linspace(min(X), max(X), 100)
+y_pred = slope * x_range + intercept
+
+# Инициализация Dash
+app = dash.Dash(__name__)
+
+# Создание макета
 app.layout = html.Div([
-    html.H1(children='AWWWWW', style={'textAlign':'center'}),
-    dcc.Dropdown(df.country.unique(), 'Ci', id='dropdown-selection'),
-    dcc.Graph(id='graph-content')
+    dcc.Graph(
+        id='scatter-plot',
+        figure={
+            'data': [
+                {'x': X.flatten(), 'y': y.flatten(), 'mode': 'markers', 'name': 'Data'},
+                {'x': x_range, 'y': y_pred, 'mode': 'lines', 'name': 'Regression Line'}
+            ],
+            'layout': {
+                'title': 'Простая линейная регрессия',
+                'xaxis': {'title': 'X'},
+                'yaxis': {'title': 'Y'}
+            }
+        }
+    )
 ])
 
-@callback(
-    Output('graph-content', 'figure'),
-    Input('dropdown-selection', 'value')
-)
-def update_graph(value):
-    dff = df[df.country==value]
-    return px.line(dff, x='year', y='pop')
-
+# Запуск приложения Dash
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    app.run_server(debug=True)
