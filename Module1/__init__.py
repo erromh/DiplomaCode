fe5b6c8e48561aaad36f6e83212def0fca168ea6
errom <errommore995@gmail.com>
2024-02-11 19:47:30 +0600

    authentication

--- Module1/__init__.py
+++ Module1/__init__.py
@@ -5,11 +5,9 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-
-    app.config['SECRET_KEY'] = 'secret-key-goes-here'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
     db.init_app(app)
+
 
     # blueprint for auth routes in our app
     from .auth import auth as auth_blueprint
@@ -18,5 +16,10 @@ def create_app():
     # blueprint for non-auth parts of app
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    # from . import models
+
+    with app.app_context():
+        db.create_all()
 
     return app
