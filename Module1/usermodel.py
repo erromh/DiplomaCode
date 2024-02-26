6060fc1194fe4b3101fbfd3e9346e6db360cab03
errommore995@gmail.com <errommore995@gmail.com>
2024-02-26 21:34:50 +0600

    new update

--- Module1/usermodel.py
+++
@@ -1,9 +0,0 @@
-from flask_login import UserMixin
-from Module1 import db
-
-# Fix it
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
-    name = db.Column(db.String(1000))
