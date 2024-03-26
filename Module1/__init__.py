958907baaa7de7a198c13abf00014c3201ba3a89
erromh <erromh@github.com>
2024-03-26 12:19:34 +0600

    S

--- Module1/__init__.py
+++ Module1/__init__.py
@@ -7,6 +7,7 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
 
+
     app.config['SECRET_KEY'] = 'secret-key-goes-here'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 
