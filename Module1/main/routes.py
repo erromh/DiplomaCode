b41366230a903cbe78f01af0a62af05a909b6e73
errom <erromh>
2024-05-28 17:06:41 +0600

    .

--- Module1/main/routes.py
+++ Module1/main/routes.py
@@ -1,11 +1,19 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import login_required
 
 main = Blueprint('main', __name__)
-
 
 @main.route('/')
 @main.route('/index')
 @login_required
 def index():
     return render_template('main/index.html')
+
+
+
+@main.route('/mode_selection', methods=['POST'])
+def mode_selection():
+    selected_option = request.form.get('option')
+    return render_template('main/result.html', selected_option=selected_option)
+
+        
