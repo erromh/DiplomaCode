0ddc97f11a3a62259d0b0a3e62b1840598ee44df
errom <erromh>
2024-06-01 19:13:04 +0600

    Добавление данных для режима обучения

--- Module1/main/routes.py
+++ Module1/main/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request
 from flask_login import login_required
+import numpy as np
 
 main = Blueprint('main', __name__)
 
@@ -10,10 +11,28 @@ def index():
     return render_template('main/index.html')
 
 
-
 @main.route('/mode_selection', methods=['POST'])
 def mode_selection():
-    selected_option = request.form.get('option')
-    return render_template('main/result.html', selected_option=selected_option)
+    if request.method == 'POST':
+        selected_option = request.form.get('option')
+        
+        if selected_option == 'Обучение':
+            # Генерация данных для режима "Обучение"
+            num_factors = np.random.randint(1, 4)  # Количество переменных-факторов: 1, 2 или 3
+            num_observations = np.random.randint(10, 31)  # Количество наблюдений: от 10 до 30
+            
+            # Создание выборки данных
+            data = np.random.rand(num_observations, num_factors)
+            data_str = '\n'.join(['\t'.join(map(str, row)) for row in data])
+            
+            selected_option = f"Генерированные данные для {num_factors} факторов и {num_observations} наблюдений:\n{data_str}"
+        
+        return render_template('main/index.html', selected_option=selected_option)
+    
+    return render_template('main/index.html', selected_option=None)
+
+
+
+# return render_template('main/result.html', selected_option=selected_option)
 
         
