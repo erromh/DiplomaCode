70d1f0fdb571612a4fd2738379e0ff0353fc692d
errom <erromh>
2024-06-04 19:41:26 +0600

    interface update

--- Module1/main/routes.py
+++ Module1/main/routes.py
@@ -11,28 +11,29 @@ def index():
     return render_template('main/index.html')
 
 
-@main.route('/mode_selection', methods=['POST'])
+@main.route('/mode_selection', methods=['GET', 'POST'])
 def mode_selection():
+
     if request.method == 'POST':
         selected_option = request.form.get('option')
         
         if selected_option == 'Обучение':
+            
             # Генерация данных для режима "Обучение"
             num_factors = np.random.randint(1, 4)  # Количество переменных-факторов: 1, 2 или 3
             num_observations = np.random.randint(10, 31)  # Количество наблюдений: от 10 до 30
             
             # Создание выборки данных
             data = np.random.rand(num_observations, num_factors)
-            data_str = '\n'.join(['\t'.join(map(str, row)) for row in data])
             
-            selected_option = f"Генерированные данные для {num_factors} факторов и {num_observations} наблюдений:\n{data_str}"
+            # Преобразование данных в строку по столбцам
+            columns = ['\n'.join(map(str, data[:, i])) for i in range(num_factors)]
+            result_text = '\n\n'.join(columns)
+
+            selected_option = f"\nГенерированные данные для {num_factors} факторов и {num_observations} наблюдений: {result_text}"
         
         return render_template('main/index.html', selected_option=selected_option)
     
     return render_template('main/index.html', selected_option=None)
-
-
-
-# return render_template('main/result.html', selected_option=selected_option)
 
         
